226dc66d4eac18c2b131fa2e872f1c8d04b16000
etbcf <tarik.edu2@gmail.com>
2025-04-25 17:08:37 +0100

    Testing loops

---
+++ Scratch Work/loops.py
@@ -0,0 +1,20 @@
+# What does this print? Why?
+"""for i in range(3):
+    print("a")
+for j in range(3):
+    print("b")"""
+
+
+# What does this print? Why?
+""" for i in range(3):
+    print("a")
+    for j in range(3):
+        print("b")
+
+print("Done") """
+
+
+# Loop from 1:00 to 12:59
+for hour in range(1, 13):
+    for minute in range(60):
+        print(hour, minute)
