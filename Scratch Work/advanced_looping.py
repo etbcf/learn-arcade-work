3161da44ae68a8dc920f6cf760767b23d947ae98
etbcf <tarik.edu2@gmail.com>
2025-04-30 06:56:38 +0100

    Practicing loops

---
+++ Scratch Work/advanced_looping.py
@@ -0,0 +1,68 @@
+# 14.2.1. Problem 1
+# for i in range(10):
+#     print("*", end=" ")
+
+
+# 14.2.2. Problem 14.2
+# for i in range(10):
+#     print("*", end=" ")
+# print()
+# for i in range(5):
+#     print("*", end=" ")
+# print()
+# for i in range(20):
+#     print("*", end=" ")
+
+
+# 14.2.3. Problem 3
+# for i in range(10):
+#     for j in range(10):
+#         print("*", end=" ")
+#     print()
+
+
+# 14.2.4. Problem 4
+# for i in range(10):
+#     for j in range(5):
+#         print("*", end=" ")
+#     print()
+
+
+# 14.2.5. Problem 5
+# for i in range(5):
+#     for j in range(20):
+#         print("*", end=" ")
+#     print()
+
+
+# 14.2.6. Problem 6
+# for i in range(10):
+#     for j in range(10):
+#         print(j, end=" ")
+#     print()
+
+
+# 14.2.7. Problem 7
+# for i in range(10):
+#     for j in range(10):
+#         print(i, end=" ")
+#     print()
+
+
+# 14.2.8. Problem 8
+# for i in range(11):
+#     for j in range(i):
+#         print(j, end=" ")
+#     print()
+
+
+# 14.2.9. Problem 9
+# for i in range(10):
+#     for j in range(i):
+#         print(" ", end=" ")
+#     for k in range(10 - i):
+#         print(k, end=" ")
+#     print()
+
+
+# 14.2.10. Problem 10
