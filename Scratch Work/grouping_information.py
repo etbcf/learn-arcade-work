d99cc3ae1a9d0f4872414e355b1dacd05c9e35c0
etbcf <tarik.edu2@gmail.com>
2025-05-05 22:00:32 +0100

    Practicing classes

---
+++ Scratch Work/grouping_information.py
@@ -0,0 +1,77 @@
+class Address:
+    """Hold all the fields for a mailing address."""
+
+    def __init__(self):
+        """Set up the address fields."""
+        self.name = ""
+        self.line1 = ""
+        self.line2 = ""
+        self.city = ""
+        self.state = ""
+        self.zip = ""
+
+
+def main():
+    # Create an address
+    home_address = Address()
+
+    # Set the fields in the address
+    home_address.name = "John Smith"
+    home_address.line1 = "701 N. C Street"
+    home_address.line2 = "Carver Science Building"
+    home_address.city = "Indianola"
+    home_address.state = "IA"
+    home_address.zip = "50125"
+
+    # Create another address
+    vacation_home_address = Address()
+
+    # Set the fields in the address
+    vacation_home_address.name = "John Smith"
+    vacation_home_address.line1 = "1122 Main Street"
+    vacation_home_address.line2 = ""
+    vacation_home_address.city = "Panama City Beach"
+    vacation_home_address.state = "FL"
+    vacation_home_address.zip = "32407"
+
+    print("The client's main home is in " + home_address.city)
+    print("His vacation home is in " + vacation_home_address.city)
+
+
+main()
+
+
+# class Cat:
+#     def __init__(self):
+#         self.name = ""
+#         self.color = ""
+#         self.weight = 0
+#
+#     def meow(self):
+#         print("meow")
+#
+#
+# my_cat = Cat()
+#
+# my_cat.name = "Mexico"
+# my_cat.color = "Black"
+# my_cat.weight = 5
+#
+# my_cat.meow()
+
+
+# class Monster:
+#     def __init__(self):
+#         self.name = ""
+#         self.health = 100
+#
+#     def decrease_health(self, amount):
+#         self.health -= amount
+#
+#         print("Current health:", self.health)
+#         print("The animal dies if current health goes below zero")
+#
+#
+# take_health = Monster()
+# take_health.decrease_health(20)
+# take_health.decrease_health(10)
