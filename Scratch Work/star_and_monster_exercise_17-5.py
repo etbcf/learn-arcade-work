d00bd949f1bca2fe54efd1057ce52d9602d04e9f
etbcf <tarik.edu2@gmail.com>
2025-05-08 07:39:21 +0100

    Some exercises, feeling lazy

---
+++ Scratch Work/star_and_monster_exercise_17-5.py
@@ -0,0 +1,18 @@
+class Star:
+
+    def __init__(self):
+        print("A star is born!")
+
+
+new_event = Star()
+
+
+class Monster:
+
+    def __init__(self, health, name):
+        self.health = health
+        self.name = name
+
+
+monster = Monster(75, "Gooba"
+print("Health:", monster.health, "Name:", monster.name)
