697f57a1b54cb8a1aa36a62338a288aab8ad4020
etbcf <tarik.edu2@gmail.com>
2025-05-15 06:42:22 +0100

    Drew some clouds
    Switch from pip to uv and put everything on functions.

--- Scratch Work/drawing-with-functions.py
+++ Scratch Work/drawing-with-functions.py
@@ -39,6 +39,28 @@ def draw_pyramid(x, y, scale):
     )
 
 
+def draw_clouds(x, y, scale):
+    """Draw a cloud made of overlapping circles at position (x, y)."""
+
+    # Main cluster of circles
+    arcade.draw_circle_filled(x, y, 20 * scale, arcade.color.WHITE)
+    arcade.draw_circle_filled(
+        x + 25 * scale, y + 10 * scale, 20 * scale, arcade.color.WHITE
+    )
+    arcade.draw_circle_filled(x + 50 * scale, y, 20 * scale, arcade.color.WHITE)
+    arcade.draw_circle_filled(
+        x + 15 * scale, y - 10 * scale, 20 * scale, arcade.color.WHITE
+    )
+    arcade.draw_circle_filled(
+        x + 40 * scale, y - 10 * scale, 20 * scale, arcade.color.WHITE
+    )
+
+
+def draw_sun(x, y, radius):
+    """Draw the sun as a filled yellow circle at (x, y) with the given radius."""
+    arcade.draw_circle_filled(x, y, radius, arcade.color.YELLOW)
+
+
 def main():
     arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "The Pyramids")
     arcade.set_background_color(arcade.color.AIR_SUPERIORITY_BLUE)
@@ -47,6 +69,12 @@ def main():
     arcade.start_render()
 
     draw_sand()
+
+    draw_sun(700, 450, 40)
+
+    draw_clouds(200, 500, 1.0)
+    draw_clouds(350, 520, 0.7)
+    draw_clouds(100, 450, 1.3)
 
     draw_pyramid(600, 150, 0.6)  # smaller
     draw_pyramid(500, 100, 1.4)  # larger
